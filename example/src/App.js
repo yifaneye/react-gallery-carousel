398f960626e958f4100df6d33f5d2ca272c5c243
Yifan Ai <yifanai@aol.com>
2020-12-29 20:28:56 +1100

    Improve, style and name the 3 carousel examples

--- example/src/App.js
+++ example/src/App.js
@@ -3,14 +3,46 @@ import React from 'react';
 import { Carousel } from 'react-gallery-carousel';
 import 'react-gallery-carousel/dist/index.css';
 
-const App = () => {
-  const kittenImageSizes = [900, 800, 700, 600, 500, 400];
-  const images = kittenImageSizes.map((kittenImageSize) => ({
-    src: `https://placekitten.com/${kittenImageSize}/${kittenImageSize}`,
-    alt: `Kitten of size ${kittenImageSize} pixels`
-  }));
+const PackageIntroductionCarousel = ({ exampleCode }) => {
+  return (
+    <Carousel
+      infinite
+      style={{
+        width: '100vw',
+        height: '20vh'
+      }}
+    >
+      <div>
+        <h1>react-gallery-carousel</h1>
+        <p>
+          Customizable, lightweight, dependency-free React carousel component
+          with support for touch swiping and lazy loading
+        </p>
+        <a href='https://www.npmjs.com/package/react-gallery-carousel'>npm</a>
+        <span> / </span>
+        <a href='https://github.com/yifaneye/react-gallery-carousel'>GitHub</a>
+      </div>
+      <div>
+        <h2>Get Started</h2>
+        <code>npm install react-gallery-carousel --save</code>
+        <p>or</p>
+        <code>yarn add react-gallery-carousel</code>
+      </div>
+      <div>
+        <h2>Example</h2>
+        <p>The basic carousel shown below:</p>
+        <code>
+          {/*<pre>{exampleCode}</pre>*/}
+          {exampleCode}
+        </code>
+      </div>
+    </Carousel>
+  );
+};
 
-  const exampleCode = `<Carousel
+const App = () => {
+  const basicCarouselExampleCode = `<Carousel
+        images={images}
         lazy
         style={{
           width: '100vw',
@@ -18,51 +50,29 @@ const App = () => {
         }}
       />`;
 
+  const kittenImageSizes = [900, 800, 700, 600, 500, 400];
+  const images = kittenImageSizes.map((kittenImageSize) => ({
+    src: `https://placekitten.com/${kittenImageSize}/${kittenImageSize}`,
+    alt: `Kitten of size ${kittenImageSize} pixels`
+  }));
+
   return (
     <div>
-      <Carousel
-        infinite
-        style={{
-          width: '100vw',
-          height: '20vh'
-        }}
-      >
-        <div>
-          <h1>react-gallery-carousel</h1>
-          <p>
-            Customizable, lightweight, dependency-free React carousel component
-            with support for touch swiping and lazy loading
-          </p>
-          <a href='https://www.npmjs.com/package/react-gallery-carousel'>npm</a>
-          <span> / </span>
-          <a href='https://github.com/yifaneye/react-gallery-carousel'>
-            GitHub
-          </a>
-        </div>
-        <div>
-          <h2>Get Started</h2>
-          <code>npm install react-gallery-carousel --save</code>
-          <p>or</p>
-          <code>yarn add react-gallery-carousel</code>
-        </div>
-        <div>
-          <h2>Example</h2>
-          <p>The first carousel:</p>
-          <code>
-            {/*<pre>{exampleCode}</pre>*/}
-            {exampleCode}
-          </code>
-        </div>
-      </Carousel>
+      {/*basic carousel example with self-managed slides */}
+      <PackageIntroductionCarousel exampleCode={basicCarouselExampleCode} />
+
+      {/*basic carousel example*/}
       <Carousel
         images={images}
         lazy
         style={{
-          width: '100vw',
-          height: '40vh'
+          maxWidth: 'min(100vh, 1440px)',
+          height: '40vh',
+          margin: '0 auto'
         }}
       />
 
+      {/*advanced carousel example*/}
       <Carousel
         images={images}
         speed={1500} // 1500 px per second
@@ -74,8 +84,9 @@ const App = () => {
         infinite
         loop
         style={{
-          width: '100vw',
+          maxWidth: 'min(100vh, 1440px)',
           height: '40vh',
+          margin: '0 auto',
           border: '10px solid #bbb',
           overflow: 'hidden',
           borderRadius: 20,
