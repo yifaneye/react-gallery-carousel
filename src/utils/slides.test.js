0b46e22e45f4081d634903cad00f3ec876b4f352
Yifan Ai <yifanai@aol.com>
2021-01-01 23:56:12 +1100

    Update Slides class test suites and test cases to improve readability

--- src/utils/slides.test.js
+++ src/utils/slides.test.js
@@ -1,33 +1,53 @@
 import Slides from './slides';
 
-test('slides with 0 items', () => {
+describe('0 item', () => {
   const slides = new Slides([], {});
-  expect(slides.slides).toStrictEqual([]);
-  expect(slides.curIndex).toBe(null);
+  it('constructs slides', () => {
+    expect(slides.slides).toStrictEqual([]);
+  });
+  it('has no index', () => {
+    expect(slides.curIndex).toBe(null);
+  });
 });
 
 const items = [1, 2, 3, 4, 5, 6];
 
-test('slides with 6 items', () => {
-  const slides = new Slides(items, {});
-  expect(slides.slides).toStrictEqual(items);
-  expect(slides.curIndex).toBe(0);
+describe('6 items', () => {
+  const slides = new Slides(items, { rtl: false, loop: false });
+  it('constructs slides', () => {
+    expect(slides.curIndex).toBe(0);
+  });
+  it('has index', () => {
+    expect(slides.curIndex).toBe(0);
+  });
 });
 
-test('slides with 6 items, rtl', () => {
-  const slides = new Slides(items, { rtl: true });
-  expect(slides.slides).toStrictEqual([6, 5, 4, 3, 2, 1]);
-  expect(slides.curIndex).toBe(5);
+describe('6 items, rtl', () => {
+  const slides = new Slides(items, { rtl: true, loop: false });
+  it('constructs slides', () => {
+    expect(slides.slides).toStrictEqual([6, 5, 4, 3, 2, 1]);
+  });
+  it('has index', () => {
+    expect(slides.curIndex).toBe(5);
+  });
 });
 
-test('slides with 6 items, loop', () => {
-  const slides = new Slides(items, { loop: true });
-  expect(slides.slides).toStrictEqual([6, 1, 2, 3, 4, 5, 6, 1]);
-  expect(slides.curIndex).toBe(1);
+describe('6 items, loop', () => {
+  const slides = new Slides(items, { rtl: false, loop: true });
+  it('constructs slides', () => {
+    expect(slides.slides).toStrictEqual([6, 1, 2, 3, 4, 5, 6, 1]);
+  });
+  it('has index', () => {
+    expect(slides.curIndex).toBe(1);
+  });
 });
 
-test('slides with 6 items, rtl, loop', () => {
+describe('6 items, rtl, loop', () => {
   const slides = new Slides(items, { rtl: true, loop: true });
-  expect(slides.slides).toStrictEqual([1, 6, 5, 4, 3, 2, 1, 6]);
-  expect(slides.curIndex).toBe(6);
+  it('constructs slides', () => {
+    expect(slides.slides).toStrictEqual([1, 6, 5, 4, 3, 2, 1, 6]);
+  });
+  it('has index', () => {
+    expect(slides.curIndex).toBe(6);
+  });
 });
