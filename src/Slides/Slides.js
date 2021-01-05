9b23993d1ceaae5d1387df61633ed1af4a93551d
Yifan Ai <yifanai@aol.com>
2021-01-05 20:39:37 +1100

    Add prop type checking for Slides component

--- src/Slides/Slides.js
+++ src/Slides/Slides.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Slide } from '../Slide/Slide';
+import PropTypes from 'prop-types';
 
 export const Slides = (props) => {
   const slides = props.slides;
@@ -8,4 +9,9 @@ export const Slides = (props) => {
   return slides.map((slide, index) => (
     <Slide key={index} {...props} slide={slide} isImage={hasImages} />
   ));
+};
+
+Slides.propTypes = {
+  slides: PropTypes.array.isRequired,
+  images: PropTypes.array
 };
