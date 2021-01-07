9df0eaad7d97a920c7881d6dd59eb46e1465b418
Yifan Ai <yifanai@aol.com>
2021-01-07 21:59:13 +1100

    Make 2 SVG icons for left and right arrows and update icon usages

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -6,6 +6,10 @@ import PropTypes from 'prop-types';
 
 export const ArrowButton = (props) => {
   const className = `${styles.buttonWrapper} ${styles[props.direction]}`;
+  const icons = {
+    left: `url("data:image/svg+xml,%3Csvg height='40' preserveAspectRatio='xMidYMid meet' viewBox='0 0 20 40' width='20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m20 0v40l-20-20z' fill='%23eee' fill-opacity='.5'/%3E%3C/svg%3E")`,
+    right: `url("data:image/svg+xml,%3Csvg height='40' preserveAspectRatio='xMidYMid meet' viewBox='0 0 20 40' width='20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 40v-40l20 20z' fill='%23eee' fill-opacity='.5'/%3E%3C/svg%3E")`
+  };
   return (
     <div className={className}>
       <Button
@@ -15,7 +19,7 @@ export const ArrowButton = (props) => {
           width: 20,
           height: 40,
           backgroundColor: 'transparent',
-          backgroundImage: `url("data:image/svg+xml,%3Csvg height='40' preserveAspectRatio='xMidYMid meet' viewBox='0 0 10 20' width='20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 20-10 10v-20-20l10 10 10 10z' fill='%23eee' fill-opacity='0.5'/%3E%3C/svg%3E%0A")`,
+          backgroundImage: icons[props.direction],
           backgroundSize: '20px 40px',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat'
