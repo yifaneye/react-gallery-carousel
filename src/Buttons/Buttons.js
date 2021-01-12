7bd07495b01cbdd02450eaff64ed387c64b50d08
Yifan Ai <yifanai@aol.com>
2021-01-12 22:01:04 +1100

    Refactor Button component and Buttons component, and introduce IconButton component to replace Button component

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -1,14 +1,27 @@
 import React, { Fragment, memo } from 'react';
-import { ArrowButton, MediaButton } from '../Button/Button';
 import PropTypes from 'prop-types';
+import { IconButton } from '../IconButton/IconButton';
+import styles from './Buttons.module.css';
 
 export const ArrowButtons = memo((props) => {
   if (props.disabled) return null;
 
   return (
     <Fragment>
-      <ArrowButton direction='left' clickCallback={props.onClickLeft} />
-      <ArrowButton direction='right' clickCallback={props.onClickRight} />
+      <div className={styles.buttonWrapper + ' ' + styles.centerLeft}>
+        <IconButton
+          name='left'
+          label='Previous Slide'
+          clickCallback={props.onClickLeft}
+        />
+      </div>
+      <div className={styles.buttonWrapper + ' ' + styles.centerRight}>
+        <IconButton
+          name='right'
+          label='Next Slide'
+          clickCallback={props.onClickRight}
+        />
+      </div>
     </Fragment>
   );
 });
@@ -23,10 +36,13 @@ export const MediaButtons = (props) => {
   if (props.disabled) return null;
 
   return (
-    <MediaButton
-      isPlaying={props.isPlaying}
-      clickCallback={props.clickCallback}
-    />
+    <div className={styles.buttonWrapper + ' ' + styles.topLeft}>
+      <IconButton
+        name={props.isPlaying ? 'pause' : 'play'}
+        label={props.isPlaying ? 'Pause Autoplay' : 'Start Autoplay'}
+        clickCallback={props.clickCallback}
+      />
+    </div>
   );
 };
 
---
+++ src/IconButton/IconButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './IconButton.module.css';
+import PropTypes from 'prop-types';
+
+export const IconButton = (props) => {
+  return (
+    <button
+      className={styles.button + ' ' + styles[props.name]}
+      aria-label={props.label}
+      aria-disabled={false}
+      onClick={props.clickCallback}
+    />
+  );
+};
+
+IconButton.propTypes = {
+  name: PropTypes.oneOf(['left', 'right', 'play', 'pause']),
+  label: PropTypes.string,
+  clickCallback: PropTypes.func
+};
