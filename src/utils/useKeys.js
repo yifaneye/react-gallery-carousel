36cf077b65878372ffe81f62834d78bdbda2482b
Yifan Ai <yifanai@aol.com>
2021-01-15 01:19:04 +1100

    Move event callback out of the Effect hook

--- src/utils/useKeys.js
+++ src/utils/useKeys.js
@@ -1,18 +1,19 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 const useKeys = (elementRef, callbacks) => {
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      callbacks[e.key] && callbacks[e.key]();
-    };
+  const handleKeyDown = useCallback(
+    (e) => callbacks[e.key] && callbacks[e.key](),
+    [callbacks]
+  );
 
+  useEffect(() => {
     const element = elementRef.current;
 
     element.addEventListener('keydown', handleKeyDown);
     return () => {
       element.removeEventListener('keydown', handleKeyDown);
     };
-  }, [elementRef, callbacks]);
+  }, [elementRef, handleKeyDown]);
 };
 
 export default useKeys;
