3600b39faed6f60ce0820b96f97b559fe08ff03d
Yifan Ai <yifanai@aol.com>
2021-01-24 18:18:21 +1100

    Update snapshot and test cases for Carousel component

--- src/components/Carousel/Carousel.test.js
+++ src/components/Carousel/Carousel.test.js
@@ -2,25 +2,30 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Carousel from '../Carousel';
 
-test('Carousel renders correctly', () => {
-  const kittenImageSizes = [900, 800, 700];
-  const images = kittenImageSizes.map((kittenImageSize) => ({
-    src: `https://placekitten.com/${kittenImageSize}/${kittenImageSize}`,
-    alt: `Kitten of size ${kittenImageSize} pixels`
-  }));
+const kittenImageSizes = [900, 800, 700];
+const images = kittenImageSizes.map((kittenImageSize) => ({
+  src: `https://placekitten.com/${kittenImageSize}/${kittenImageSize}`,
+  alt: `Kitten of size ${kittenImageSize} pixels`
+}));
 
-  Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation((query) => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn()
-    }))
+describe('Carousel', () => {
+  it('is truthy', () => {
+    expect(Carousel).toBeTruthy();
   });
 
-  const component = renderer.create(<Carousel images={images} />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  it('renders the same as snapshot', () => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn()
+      }))
+    });
+    const component = renderer.create(<Carousel images={images} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
