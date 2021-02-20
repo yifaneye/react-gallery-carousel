61360be294ae4f77595cb7bb560f54d03ef075a2
Yifan Ai <yifanai@aol.com>
2021-02-20 21:35:05 +1100

    Make decision on swipe direction to improve UX when swiping on maximized carousel (both horizontally and vertically)

--- src/utils/useSwipe.js
+++ src/utils/useSwipe.js
@@ -15,34 +15,45 @@ const useSwipe = (
     onTap
   }
 ) => {
+  let isInitialSwipeDown;
+
   const handleSwipeEnd = (displacementX, displacementY = 0, velocity = 0) => {
     const { clientWidth: width, clientHeight: height } = elementRef.current;
     const distanceXMin = width * swipePercentageMin;
     const distanceYMin = height * swipePercentageMin;
     const speed = Math.abs(velocity);
     if (
-      displacementX <= -Math.abs(displacementY) &&
+      !isInitialSwipeDown &&
+      // displacementX <= -Math.abs(displacementY) &&
       displacementX <= -distanceXMin
     )
       onSwipeEndLeft(displacementX, speed);
     else if (
-      displacementX >= Math.abs(displacementY) &&
+      !isInitialSwipeDown &&
+      // displacementX >= Math.abs(displacementY) &&
       displacementX >= distanceXMin
     )
       onSwipeEndRight(displacementX, speed);
     else if (
-      Math.abs(displacementX) < Math.abs(displacementY) &&
+      isInitialSwipeDown &&
+      // Math.abs(displacementX) < displacementY &&
       displacementY >= distanceYMin
     )
       onSwipeEndDown();
     else onSwipeEndDisqualified(displacementX, speed);
+    isInitialSwipeDown = undefined;
   };
 
   const handleSwipeMove = (displacementX, displacementY = 0) => {
-    if (Math.abs(displacementX) >= Math.abs(displacementY))
-      onSwipeMoveX(displacementX);
-    else if (Math.abs(displacementX) < displacementY)
-      onSwipeMoveDown(displacementX, displacementY);
+    if (isInitialSwipeDown === false) onSwipeMoveX(displacementX);
+    else if (isInitialSwipeDown) onSwipeMoveDown(displacementX, displacementY);
+    else {
+      // when isInitialSwipeDown is undefined
+      if (Math.abs(displacementX) >= displacementY) isInitialSwipeDown = false;
+      else if (Math.abs(displacementX) < displacementY)
+        isInitialSwipeDown = true;
+      handleSwipeMove(displacementX, displacementY);
+    }
   };
 
   // have to use event listeners (active event listeners) to deal with undesired tiny vertical movements
