62b9c1f5a49384e655d17b9589edf31b5950d616
Yifan Ai <yifanai@aol.com>
2021-02-22 21:30:55 +1100

    Fix issue where slides transition stuck when cursor enters caption, and simplify useNoSwipe hook

--- src/utils/useNoSwipe.js
+++ src/utils/useNoSwipe.js
@@ -7,23 +7,13 @@ const useNoSwipe = (elementRef) => {
 
     if (el) {
       el.addEventListener('mousedown', handleSwipe);
-      el.addEventListener('mousemove', handleSwipe);
-      el.addEventListener('mouseup', handleSwipe);
       el.addEventListener('touchstart', handleSwipe, { passive: true });
-      el.addEventListener('touchmove', handleSwipe, { passive: true });
-      el.addEventListener('touchend', handleSwipe, { passive: true });
-      el.addEventListener('touchcancel', handleSwipe, { passive: true });
     }
 
     return () => {
       if (!el) {
         el.removeEventListener('mousedown', handleSwipe);
-        el.removeEventListener('mousemove', handleSwipe);
-        el.removeEventListener('mouseup', handleSwipe);
         el.removeEventListener('touchstart', handleSwipe, { passive: true });
-        el.removeEventListener('touchmove', handleSwipe, { passive: true });
-        el.removeEventListener('touchend', handleSwipe, { passive: true });
-        el.removeEventListener('touchcancel', handleSwipe, { passive: true });
       }
     };
   }, [elementRef]);
