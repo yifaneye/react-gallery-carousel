369d4b261fed70fcec1637fce8a6d2374e9fa706
bux <33053305>
2021-06-16 18:23:01 +0300

    Fix Iphone Compatibility Issue

--- src/utils/useMediaQuery.js
+++ src/utils/useMediaQuery.js
@@ -6,9 +6,13 @@ const useMediaQuery = (query) => {
 
   useEffect(() => {
     const callback = () => setMatches(mediaQueryList.matches);
-    mediaQueryList.addEventListener && mediaQueryList.addEventListener('change', callback);
+    mediaQueryList.addEventListener
+      ? mediaQueryList.addEventListener('change', callback)
+      : window.addEventListener('resize', callback);
 
-    return () => mediaQueryList.addEventListener && mediaQueryList.removeEventListener('change', callback);
+    return () => mediaQueryList.addEventListener
+      ? mediaQueryList.removeEventListener('change', callback)
+      : window.removeEventListener('resize', callback);
   }, [mediaQueryList]);
 
   return matches;
