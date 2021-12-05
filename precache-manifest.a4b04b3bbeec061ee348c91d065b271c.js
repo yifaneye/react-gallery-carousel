ecfb539a0c45719708c814d08930428299027005
Yifan Ai <yifanai@aol.com>
2021-12-05 15:25:37 +1100

    Updates

---
+++ precache-manifest.a4b04b3bbeec061ee348c91d065b271c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff66d03d3bd37c53458900b59e0990a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1086f826228903a9f9e6",
+    "url": "./static/css/2.ccdd70e8.chunk.css"
+  },
+  {
+    "revision": "1fc9d61e97f0f4c6702a",
+    "url": "./static/css/main.18c7792b.chunk.css"
+  },
+  {
+    "revision": "1086f826228903a9f9e6",
+    "url": "./static/js/2.6aeddfc7.chunk.js"
+  },
+  {
+    "revision": "68e052edda21b10d5800e8854a7bfc38",
+    "url": "./static/js/2.6aeddfc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc9d61e97f0f4c6702a",
+    "url": "./static/js/main.1b445286.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.1b445286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1334bfaa42e173278283",
+    "url": "./static/js/runtime-main.7fcac96b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.379dcee998cf4b84e5128b3489bc6a63.js"
+  "./precache-manifest.a4b04b3bbeec061ee348c91d065b271c.js"
 );
 
 self.addEventListener('message', (event) => {
