c732cddc3a79eaf3d3eac48938596b1abe344263
Yifan Ai <yifanai@aol.com>
2021-12-23 16:10:07 +1100

    Simplify Gatsby example

--- react-gallery-carousel-gatsby/gatsby-config.js
+++ react-gallery-carousel-gatsby/gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'react-gallery-carousel Gatsby example',
     description:
       'react-gallery-carousel is a mobile-friendly dependency-free React carousel component with support for touch, mouse dragging, lazy loading, thumbnails, modal, keyboard navigation, RTL and pinch to zoom.',
-    url: 'https://rgc.yifanai.com',
+    url: 'https://yifanai.com/rgc',
     image: '/images/icon.png',
     twitterUsername: '@yifaneye'
   },
